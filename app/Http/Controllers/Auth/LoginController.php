23e5285f53e7cb0ddfccd353dddb02ac4d97fe4c
Kusuma Wecitra <kwecitra@gmail.com>
2022-03-31 19:28:21 +0700

    fix login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+// use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     /*
@@ -21,12 +22,7 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
     
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -36,6 +32,23 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request) {
+        $input = $request->all();
+        $this->validate($request, [
+            'nik' => 'required',
+            'password' => 'required',
+        ]);
+        $fieldType = filter_var($request->nik, FILTER_VALIDATE_EMAIL) ? 'email' : 'nik';
+        if(auth()->attempt(array(
+            $fieldType => $input['nik'],
+            'password' => $input['password']
+        ))){
+            return redirect()->route('home');
+        } else {
+            return redirect()->route('login')->with('gagal', 'NIK dan Password salah');
+        }
     }
 
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,26 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if(session()->has('gagal'))
+                <div class="alert alert-danger alert-dismissible" role="alert">
+                    {{ session('gagal') }}
+                </div>
+            @endif
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        {{-- <div class="row mb-3">
-                            <label for="nik" class="col-md-4 col-form-label text-md-end">{{ __('NIK') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="nik" type="text" class="form-control @error('nik') is-invalid @enderror" name="login" value="{{ old('nik') }}" required autocomplete="nik" autofocus>
-
-                                @error('nik')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div> --}}
 
                         <div class="row mb-3">
                             <label for="nik" class="col-md-4 col-form-label text-md-end">{{ __('NIK Address') }}</label>
